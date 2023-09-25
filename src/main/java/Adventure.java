64ffb1f32afaefa16a57104a67ad3b44723d865b
koak0001 <koak0001@stud.kea.dk>
2023-09-25 14:38:31 +0200

    Adventure changes

--- src/main/java/Adventure.java
+++ src/main/java/Adventure.java
@@ -1,13 +1,13 @@
 public class Adventure {
     private Room currentRoom;
-    Room room1 = new Room("","")
-    Room room2 = new Room("","")
-    Room room3 = new Room("","")
-    Room room4 = new Room("","")
-    Room room5 = new Room("","")
-    Room room6 = new Room("","")
-    Room room7 = new Room("","")
-    Room room8 = new Room("", "")
+    Room room1 = new Room("", "");
+    Room room2 = new Room("", "");
+    Room room3 = new Room("", "");
+    Room room4 = new Room("", "");
+    Room room5 = new Room("", "");
+    Room room6 = new Room("", "");
+    Room room7 = new Room("", "");
+    Room room8 = new Room("", "");
 
     public Adventure() {
     }
@@ -19,17 +19,20 @@ public class Adventure {
 
 
     }
-public void look(){
-    System.out.println("You are standing in " + currentRoom.getRoomName()+ currentRoom.getDescription());
-}
-public void moveSouth(Room southRoom){
-if (currentRoom.equals(southRoom)) {
+
+    public void look() {
+        System.out.println("You are standing in " + currentRoom.getRoomName() + currentRoom.getDescription());
+    }
+
+    public void moveSouth(Room southRoom) {
+        if (currentRoom.equals(southRoom)) {
             currentRoom = currentRoom.getSouthRoom();
         } else {
             currentRoom = currentRoom.getSouthRoom();
         }
 
 
+    }
 }
 
 
