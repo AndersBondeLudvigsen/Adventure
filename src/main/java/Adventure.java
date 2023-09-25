0b74302e509641f178aec216e1f4f847572bfedf
AndersBondeLudvigsen <anlu0001@stud.kea.dk>
2023-09-25 14:33:28 +0200

    Commit 4

--- src/main/java/Adventure.java
+++ src/main/java/Adventure.java
@@ -1,6 +1,13 @@
 public class Adventure {
     private Room currentRoom;
-
+    Room room1 = new Room("","")
+    Room room2 = new Room("","")
+    Room room3 = new Room("","")
+    Room room4 = new Room("","")
+    Room room5 = new Room("","")
+    Room room6 = new Room("","")
+    Room room7 = new Room("","")
+    Room room8 = new Room("", "")
 
     public Adventure() {
     }
@@ -8,13 +15,14 @@ public class Adventure {
 
     public void buildRoom() {
         this.currentRoom = new Room("Room 5",
-            " A derelict tailor's workshop, covered in cobwebs");
+                " A derelict tailor's workshop, covered in cobwebs");
 
 
     }
-public void look(){
-    System.out.println("You are standing in " + currentRoom.getRoomName()+ currentRoom.getDescription());
-}
+
+    public void look() {
+        System.out.println("You are standing in " + currentRoom.getRoomName() + currentRoom.getDescription());
+    }
 /*public void moveSouth(Room southRoom){
 if (room.equals("mother")) {
             current = current.getMother();
